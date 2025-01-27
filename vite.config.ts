964b89e4b5a6c9d770979443b54ed085a3ddf59f
Danny Phillips <danny.phillips8@gmail.com>
2025-01-26 20:30:36 -0800

    fixed builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/scoreboard/',
+  base: process.env.NODE_ENV === 'production' ? '/scoreboard/' : '/',
   server: {
     port: 5173
   }
